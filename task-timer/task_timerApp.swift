c627ff7c3fe22e1fe3fcfc951c68f27d9609b514
zysang <zysang@trip.com>
2025-10-21 11:34:54 +0800

    Initial Commit

---
+++ task-timer/task_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  task_timerApp.swift
+//  task-timer
+//
+//  Created by zysang on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct task_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
